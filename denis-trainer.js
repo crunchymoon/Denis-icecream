573611904e7b49d70878145220a4f4c05523c5a0
Denis <sldenys96@hotmail.com>
2021-03-04 21:30:29 +0100

    created + designed the carousel

--- denis-trainer.js
+++ denis-trainer.js
@@ -1,39 +1,74 @@
-const carouselSlide = document.querySelector('.carousel__slide');
-const carouselImages = document.querySelectorAll('.carousel__slide img');
+window.addEventListener('load', function () {
+    const carouselSlide = document.querySelector('.carousel__slide');
+    const carouselImages = document.querySelectorAll('.carousel__slide img');
+    const carouselDots = document.querySelector('.carousel__dots');
+    const carouselDot = document.querySelectorAll('.carousel__dots--dot');
 
-//Buttons
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
+    //Buttons
+    const prevBtn = document.querySelector('#prevBtn');
+    const nextBtn = document.querySelector('#nextBtn');
 
-//Counter
-let counter = 1;
-const size = carouselImages[0].clientWidth;
-console.log(size);
+    //Counter
+    let counter = 1;
+    const size = carouselImages[0].clientWidth;
+    console.log(size);
+//    carouselSlide.style.transform = `translateX(${(-size*counter)}px)`;
+    carouselImages[counter].style.transform = 'scale(1)';
+    carouselImages[counter].style.transition = '0';
 
-carouselSlide.style.transform = `translateX(${(-size*counter)}px)`;
-
-//Button Listeners
-nextBtn.addEventListener('click', (e) => {
-    if(counter>=carouselImages.length-1)return;
-    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-    counter++;
-    carouselSlide.style.transform = `translateX(${(-size*counter)}px)`;
-});
-prevBtn.addEventListener('click',(e)=>{
-    if(counter<=0)return;
-    carouselSlide.style.transition='transform 0.4s ease-in-out';
-    counter--;
-    carouselSlide.style.transform=`translateX(${(-size*counter)}px)`;
-});
-carouselSlide.addEventListener('transitionend',()=>{
-    if(carouselImages[counter].id==='lastClone'){
-        carouselSlide.style.transition = 'none';
-        counter=carouselImages.length-2;
+    nextBtn.addEventListener('click', (e) => {
+        if (counter >= carouselImages.length - 1) return;
+        carouselSlide.style.transition = 'transform 0.15s ease';
+        counter++;
         carouselSlide.style.transform = `translateX(${(-size*counter)}px)`;
-       }
-    if(carouselImages[counter].id==='firstClone'){
-        carouselSlide.style.transition = 'none';
-        counter=carouselImages.length-counter;
+        if (carouselImages[counter].id == 'lastImg') {
+            nextBtn.style.opacity = '0';
+        }
+        if (prevBtn.style.opacity = '0') {
+            prevBtn.style.opacity = '1';
+        };
+    });
+
+    prevBtn.addEventListener('click', (e) => {
+        if (counter <= 0) return;
+        carouselSlide.style.transition = 'transform 0.15s ease';
+        counter--;
         carouselSlide.style.transform = `translateX(${(-size*counter)}px)`;
-       }  
-})
+        if (carouselImages[counter].id == 'firstImg') {
+            prevBtn.style.opacity = '0';
+        }
+        if (nextBtn.style.opacity = '0') {
+            nextBtn.style.opacity = '1';
+        };
+    });
+    carouselSlide.addEventListener('transitionend', (e) => {
+        carouselImages[counter].style.transform = 'scale(1)';
+        carouselImages.forEach((carouselImg) => {
+            carouselImg.style.transform = 'scale(.8)';
+            if (carouselImages[counter] = carouselImg) {
+                carouselImages[counter].style.transform = 'scale(1)';
+            }
+        })
+    })
+
+    carouselDot[counter].style.transform = 'scale(1.5)';
+    carouselDot[counter].style.transition = '0';
+    prevBtn.addEventListener('click', (e) => {
+        carouselDot[counter].style.transform = 'scale(1.5)';
+        carouselDot[counter].style.transition = 'transform 0.3s ease';
+        carouselDot.forEach((carDot) => {
+            if (carDot != carouselDot[counter]) {
+                carDot.style.transform = 'scale(1)';
+            }
+        })
+    });
+    nextBtn.addEventListener('click', (e) => {
+        carouselDot[counter].style.transform = 'scale(1.5)';
+        carouselDot[counter].style.transition = 'transform 0.3s ease';
+        carouselDot.forEach((carDot) => {
+            if (carDot != carouselDot[counter]) {
+                carDot.style.transform = 'scale(1)';
+            }
+        })
+    });
+});
